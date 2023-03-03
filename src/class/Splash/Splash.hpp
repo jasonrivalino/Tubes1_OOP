0ce7ac36b5891bae2d8243bcb2d3e5302e0dd22b
Jason Rivalino <jasonrivalino@gmail.com>
2023-03-03 16:20:14 +0700

    add splash screen cuy

---
+++ src/class/Splash/Splash.hpp
@@ -0,0 +1,13 @@
+#ifndef SPLASH_HPP
+#define SPLASH_HPP
+
+#include <iostream>
+using namespace std;
+
+class Splash{
+    public:
+    void SplashOpening();
+    void SplashClosing();
+};
+
+#endif
