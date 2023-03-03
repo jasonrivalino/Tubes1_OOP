f5ae403aca51d2fe0ec43a2b2cacb6d89e72fb84
Jason Rivalino <jasonrivalino@gmail.com>
2023-03-03 13:41:14 +0700

    nambah kondisi gapunya kartu

--- src/class/Player/PlayerAction/PlayerAction.cpp
+++ src/class/Player/PlayerAction/PlayerAction.cpp
@@ -25,23 +25,41 @@ void PlayerAction::nextCard(){
 }
 
 void PlayerAction::reRoll(Player &p,SetGame &s){
-    cout << "Melakukan pembuangan kartu yang sedang dimiliki" << endl;
-    cout << "Kamu mendapatkan 2 kartu baru yaitu:" << endl;
-    ReRoll reRollCard;
-    reRollCard.ReRollEffect(p,s);
+    if (p.getCardsPlayer()[2].getNameCard() != "REROLL" || p.getCardsPlayer()[2].getIsCardUsed()){
+        cout << "Ets, tidak bisa. Kamu tidak punya kartu Ability REROLL." << endl;
+    }
+    else{
+        cout << "Melakukan pembuangan kartu yang sedang dimiliki" << endl;
+        cout << "Kamu mendapatkan 2 kartu baru yaitu:" << endl;
+        ReRoll reRollCard;
+        reRollCard.ReRollEffect(p,s);
+    }
 }
 
 void PlayerAction::quadrupleCard(Player &player, SetGame &pointGame){
-    Quadruple quadrupleCard;
-    long pointBeforeAction = pointGame.getPointGame();
-    pointGame.setPoint(pointGame.getPointGame()*quadrupleCard.getQuadruple());
-    cout << &player << "melakukan QUADRUPLE!" << "Poin hadiah naik dari " << pointBeforeAction << " menjadi " << pointGame.getPointGame() << endl;
+    // Kondisi jika pemain tidak punya kartu quadruple
+    if (player.getCardsPlayer()[2].getNameCard() != "QUADRUPLE" || player.getCardsPlayer()[2].getIsCardUsed()){
+        cout << "Ets, tidak bisa. Kamu tidak punya kartu Ability QUADRUPLE." << endl;
+    }
+    // Kondisi jika pemain punya kartu quadruple
+    else{    
+        Quadruple quadrupleCard;
+        long pointBeforeAction = pointGame.getPointGame();
+        pointGame.setPoint(pointGame.getPointGame()*quadrupleCard.getQuadruple());
+        cout << &player << "melakukan QUADRUPLE!" << "Poin hadiah naik dari " << pointBeforeAction << " menjadi " << pointGame.getPointGame() << endl;
+    }
 }
 
 void PlayerAction::quarterCard(Player &player, SetGame &pointGame){
-    Quarter quarterCard;
-    long pointBeforeAction = pointGame.getPointGame();
-    pointGame.setPoint(pointGame.getPointGame()/quarterCard.getQuarter());
-    cout << &player << "melakukan QUARTER!" << "Poin hadiah turun dari " << pointBeforeAction << " menjadi " << pointGame.getPointGame() << endl;
+    // Kondisi jika pemain tidak punya kartu quarter
+    if (player.getCardsPlayer()[2].getNameCard() != "QUARTER" || player.getCardsPlayer()[3].getIsCardUsed()){
+        cout << "Ets, tidak bisa. Kamu tidak punya kartu Ability QUARTER." << endl;
+    }
+    else{
+        Quarter quarterCard;
+        long pointBeforeAction = pointGame.getPointGame();
+        pointGame.setPoint(pointGame.getPointGame()/quarterCard.getQuarter());
+        cout << &player << "melakukan QUARTER!" << "Poin hadiah turun dari " << pointBeforeAction << " menjadi " << pointGame.getPointGame() << endl;
+    }
 }
 
